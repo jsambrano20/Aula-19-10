9ad3c7b3df59d6ba82add064170254be6e67ca82
SAMBRANO <joao.sambrano@atos.net>
2021-10-20 18:56:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+namespace ConsoleApp_Aula_19_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ctrl + K + U Descomenta
+            //Alt + Setas cima e baixo
+
+            //TIPO NOME;//declara uma variavel
+            //NOME = Valor;//atribuição
+            //inteiro = 7;
+            // real = 7;
+            //int inteiro;
+            //string texto;
+            //double real;
+            //bool boleano;
+
+            //Exercicio 1
+            //double valor1;
+            //double valor2;
+            //double soma;
+            //double sub;
+            //double mult;
+            //double div;
+
+            //Console.Write("Informe o valor: ");
+            //valor1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Informe o valor: ");
+            //valor2 = double.Parse(Console.ReadLine());
+
+            //soma = valor1 + valor2;
+            //sub = valor1 - valor2;
+            //mult = valor1 * valor2;
+            //div = valor1 / valor2;
+
+
+            //Console.WriteLine("A soma resulta em: " + soma);
+            //Console.WriteLine("A subtração resultada em: " + sub);
+            //Console.WriteLine("A multiplicação resulta em: " + mult);
+            //Console.WriteLine("A divisão resulta em: " + div);
+
+
+            ////Console.ReadKey();
+
+            //exercicio 2
+            ////double F ;
+            ////double C;
+
+            ////Console.WriteLine(" Informe A Temperatura Média Do Local Em Celsius: ");
+            ////F = double.Parse(Console.ReadLine());
+
+            /////F = ((C * 9) + 160) / 5);
+
+            ////Console.WriteLine(" A Temperatura Em Graus Fahrenheit é De: " + F);
+
+            ////Console.ReadKey();
+
+            // exercio 3
+            //string nome;
+            //double horario;
+            //double valorhora;
+            //double salario;
+
+            //Console.Write("Nome do colaborador: ");
+            //nome = Console.ReadLine();
+
+            //Console.Write("Informe as horas trabalhas");
+            //horario = double.Parse(Console.ReadLine());
+
+            //Console.Write("valor por horas:");
+            //valorhora = double.Parse(Console.ReadLine());
+
+            //salario = horario * valorhora;
+
+            //Console.Write(" O salario de " + nome + "é de " + salario);
+
+            //EXERCICIO 4
+            //int x = 5;
+            //int y = 7;
+            //int aux;
+
+            //aux = x;
+            //x = y;
+            //y = aux;
+
+
+            //Console.Write(x);
+            //Console.Write(y);
+
+            //EXERCICIO 5
+            //int anos;
+            //int meses;
+            //int dias;
+
+            //Console.Write("Quantidade de Anos: ");
+            //anos = int.Parse(Console.ReadLine());
+
+            //Console.Write("Quantos Meses: ");
+            //meses = int.Parse(Console.ReadLine());
+
+            //Console.Write("Quantos dias: ");
+            //dias = int.Parse(Console.ReadLine());
+
+            //dias = (anos * 365) + (meses * 30) + dias;
+
+            //Console.Write("Resulta em " + dias + " dias! ");
+
+
+            //Exercicio 6
+
+            //double salario;
+            //double percentual;
+
+
+            //Console.WriteLine("salario atual: ");
+            //salario = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Percentual de aumento: ");
+            //percentual = double.Parse(Console.ReadLine());
+
+            //salario = salario + (salario * percentual) / 100;
+
+            //Console.Write("o Aumento resulta em: " + salario);
+
+            //EXERCICIO 7 
+
+            //double carro = 30000;
+            //double salario;
+            //string nome;
+            //int qtd;
+            //double per = 15;
+
+            //Console.WriteLine("Nome do funcionário: ");
+            //nome = Console.ReadLine();
+
+            //Console.WriteLine("Salario funcionário: ");
+            //salario = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Quantidade de carros vendidos: ");
+            //qtd = int.Parse(Console.ReadLine());
+
+            //salario = salario + per * qtd ;
+
+            //Console.WriteLine("O nome do colaborador é " + nome + " Salario final é: " + salario);
+
+            //EXERCICIO 8 
+
+            //int votosBrancos, votosBrancosp;
+            //int votosnulos, votosnulosp;
+            //int votosvalidos, votosvalidosp;
+
+
+            //Console.Write("Quantidade de votos brancos: ");
+            //votosBrancos = int.Parse(Console.ReadLine());
+
+            //Console.Write("Quantidade de votos nulos: ");
+            //votosnulos = int.Parse(Console.ReadLine());
+
+            //Console.Write("Quantidade de votos validos: ");
+            //votosvalidos = int.Parse(Console.ReadLine());
+
+            //int total = votosBrancos + votosnulos + votosvalidos;
+
+            //votosBrancosp = (votosBrancos * 100) / total;
+            //votosnulosp = (votosnulos * 100) / total;
+            //votosvalidosp = (votosvalidos * 100) / total;
+
+            //Console.WriteLine(" o Percentual de brancos é :" + votosBrancosp);
+            //Console.WriteLine(" o Percentual de nulo é :" + votosnulosp);
+            //Console.WriteLine(" o Percentual de valido é :" + votosvalidosp);
+
+            //EXERCICIO 9 // ou criar variaveis para ante e post
+
+            //int num;
+
+            //Console.Write("Numero do usuário: ");
+            //num = int.Parse(Console.ReadLine());
+
+            //num = num - 1;
+            //Console.WriteLine("Numero anterior: " + num);
+
+            //num = num + 2;
+            //Console.WriteLine("Numero posterior: " + num);
+
+
+            // Exercicio 10
+
+            //double nota1, nota2, nota3;
+            //double media;
+
+            //Console.Write("Nota 1: ");
+            //nota1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Nota 2: ");
+            //nota2 = double.Parse(Console.ReadLine()); 
+            
+            //Console.Write("Nota 3: ");
+            //nota3 = double.Parse(Console.ReadLine());
+
+            //media = (nota1 + nota2 + nota3) / 3;
+
+            //Console.Write("a media final é :" + media);
+
+
+        }
+    }
+}
